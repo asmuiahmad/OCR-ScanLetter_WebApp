d7614a972c91426ac906be5b065d99b62686411f
asmuiahmad <asmui.ahmad@muslim.com>
2025-02-01 19:37:58 +0800

    Reset All files with new one

--- README.md
+++
@@ -1,1 +0,0 @@
-<h1>This Program is still under Construction</h1>
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-python -m pip install --upgrade pip
-
-pip3 install -r requirements.txt
